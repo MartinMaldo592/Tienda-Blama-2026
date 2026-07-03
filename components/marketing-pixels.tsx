7d7e3c01aa2ca7520fc5800a1fba5c96de900635
Martin Maldonado <maldonado4250@gmail.com>
2026-07-03 16:31:05 +0000

    fix(marketing): refactor loader to support hybrid GTM variables injection and prevent double tracking

--- components/marketing-pixels.tsx
+++ components/marketing-pixels.tsx
@@ -9,11 +9,12 @@ import { createClient } from "@/lib/supabase.client"
 type ActivePixel = {
     clave: string
     pixel_id: string
+    enabled: boolean
 }
 
 export function MarketingPixels() {
     const pathname = usePathname()
-    const [activePixels, setActivePixels] = useState<ActivePixel[]>([])
+    const [pixels, setPixels] = useState<ActivePixel[]>([])
     const [loading, setLoading] = useState(true)
 
     // Si la ruta comienza con /admin, no cargamos nada de tracking
@@ -30,17 +31,9 @@ export function MarketingPixels() {
                 const supabase = createClient()
                 const { data, error } = await (supabase.from("marketing_pixels" as any) as any)
                     .select("clave, pixel_id, enabled")
-                    .eq("enabled", true)
 
                 if (!error && data) {
-                    // Solo guardamos los que tengan un pixel_id configurado (no vacíos)
-                    const validPixels = (data as any[])
-                        .filter(p => p.pixel_id && p.pixel_id.trim() !== "")
-                        .map(p => ({
-                            clave: p.clave as string,
-                            pixel_id: p.pixel_id.trim() as string
-                        }))
-                    setActivePixels(validPixels)
+                    setPixels(data as ActivePixel[])
                 }
             } catch (err) {
                 console.error("Error fetching marketing pixels:", err)
@@ -56,10 +49,14 @@ export function MarketingPixels() {
         return null
     }
 
-    const gtm = activePixels.find(p => p.clave === "gtm")
-    const facebook = activePixels.find(p => p.clave === "facebook")
-    const tiktok = activePixels.find(p => p.clave === "tiktok")
-    const ga4 = activePixels.find(p => p.clave === "ga4")
+    const gtm = pixels.find(p => p.clave === "gtm" && p.enabled && p.pixel_id && p.pixel_id.trim() !== "")
+    const facebook = pixels.find(p => p.clave === "facebook")
+    const tiktok = pixels.find(p => p.clave === "tiktok")
+    const ga4 = pixels.find(p => p.clave === "ga4")
+
+    const fbId = facebook?.pixel_id?.trim() || ""
+    const ttId = tiktok?.pixel_id?.trim() || ""
+    const gaId = ga4?.pixel_id?.trim() || ""
 
     return (
         <>
@@ -72,6 +69,11 @@ export function MarketingPixels() {
                     window.ttq.load = window.ttq.load || function() {};
                     window.ttq.page = window.ttq.page || function() {};
                     window.ttq.track = window.ttq.track || function() {};
+                    
+                    // Exponer variables globales para que GTM pueda leer los IDs dinámicamente si los requiere
+                    window.metaPixelId = "${fbId}";
+                    window.tiktokPixelId = "${ttId}";
+                    window.ga4PixelId = "${gaId}";
                 `}
             </Script>
 
@@ -80,8 +82,13 @@ export function MarketingPixels() {
                 <>
                     <Script id="gtm-script" strategy="lazyOnload">
                         {`
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({
+                                'gtm.start': new Date().getTime(),
+                                event: 'gtm.js',
+                                metaPixelId: '${fbId}',
+                                tiktokPixelId: '${ttId}',
+                                ga4PixelId: '${gaId}'
+                            });var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer','${gtm.pixel_id}');
@@ -98,50 +105,55 @@ export function MarketingPixels() {
                 </>
             )}
 
-            {/* Google Analytics 4 (GA4) */}
-            {ga4 && (
+            {/* Carga Directa (Solo si GTM NO está habilitado, para evitar doble tracking) */}
+            {!gtm && (
                 <>
-                    <Script src={`https://www.googletagmanager.com/gtag/js?id=${ga4.pixel_id}`} strategy="lazyOnload" />
-                    <Script id="ga4-script" strategy="lazyOnload">
-                        {`
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', '${ga4.pixel_id}');
-                        `}
-                    </Script>
+                    {/* Google Analytics 4 (GA4) Directo */}
+                    {ga4 && ga4.enabled && gaId && (
+                        <>
+                            <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="lazyOnload" />
+                            <Script id="ga4-script" strategy="lazyOnload">
+                                {`
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', '${gaId}');
+                                `}
+                            </Script>
+                        </>
+                    )}
+
+                    {/* Meta Pixel (Facebook) Directo */}
+                    {facebook && facebook.enabled && fbId && (
+                        <Script id="facebook-pixel" strategy="lazyOnload">
+                            {`
+                                !function(f,b,e,v,n,t,s)
+                                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                n.queue=[];t=b.createElement(e);t.async=!0;
+                                t.src=v;s=b.getElementsByTagName(e)[0];
+                                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                'https://connect.facebook.net/en_US/fbevents.js');
+                                fbq('init', '${fbId}');
+                                fbq('track', 'PageView');
+                            `}
+                        </Script>
+                    )}
+
+                    {/* TikTok Pixel Directo */}
+                    {tiktok && tiktok.enabled && ttId && (
+                        <Script id="tiktok-pixel" strategy="lazyOnload">
+                            {`
+                                !function (w, d, t) {
+                                  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var e=0;e<ttq.methods.length;e++)ttq.setAndDefer(ttq,ttq.methods[e]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.mixpanel;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var i=d.createElement("script");i.type="text/javascript",i.async=!0,i.src=r;var a=d.getElementsByTagName("script")[0];a.parentNode.insertBefore(i,a)};
+                                  ttq.load('${ttId}');
+                                  ttq.page();
+                                }(window, document, 'ttq');
+                            `}
+                        </Script>
+                    )}
                 </>
-            )}
-
-            {/* Meta Pixel (Facebook) */}
-            {facebook && (
-                <Script id="facebook-pixel" strategy="lazyOnload">
-                    {`
-                        !function(f,b,e,v,n,t,s)
-                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                        n.queue=[];t=b.createElement(e);t.async=!0;
-                        t.src=v;s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)}(window, document,'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '${facebook.pixel_id}');
-                        fbq('track', 'PageView');
-                    `}
-                </Script>
-            )}
-
-            {/* TikTok Pixel */}
-            {tiktok && (
-                <Script id="tiktok-pixel" strategy="lazyOnload">
-                    {`
-                        !function (w, d, t) {
-                          w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var e=0;e<ttq.methods.length;e++)ttq.setAndDefer(ttq,ttq.methods[e]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.mixpanel;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var i=d.createElement("script");i.type="text/javascript",i.async=!0,i.src=r;var a=d.getElementsByTagName("script")[0];a.parentNode.insertBefore(i,a)};
-                          ttq.load('${tiktok.pixel_id}');
-                          ttq.page();
-                        }(window, document, 'ttq');
-                    `}
-                </Script>
             )}
 
             {/* Atribución de UTMs y campañas */}
